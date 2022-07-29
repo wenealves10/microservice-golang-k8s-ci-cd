6006cfd6a0df9e165fe0c3611ec814e41df3dda7
Wene Alves <weneplay5@gmail.com>
2022-07-29 21:37:36 +0000

    feat: customer

--- aggregate/customer.go
+++ aggregate/customer.go
@@ -1,12 +1,38 @@
 package aggregate
 
 import (
+	"errors"
+
+	"github.com/google/uuid"
+
 	"github.com/wenealves10/microservice-golang-k8s-ci-cd/entity"
 	"github.com/wenealves10/microservice-golang-k8s-ci-cd/valueobject"
+)
+
+var (
+	ErrInvalidPerson = errors.New("a customer has to have an valid person")
 )
 
 type Customer struct {
 	person       *entity.Person
 	products     []*entity.Item
-	transactions []*valueobject.Transaction
+	transactions []valueobject.Transaction
+}
+
+func NewCustomer(name string) (Customer, error) {
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	person := &entity.Person{
+		Name: name,
+		ID:   uuid.New(),
+	}
+
+	return Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]valueobject.Transaction, 0),
+	}, nil
+
 }
---
+++ aggregate/customer_test.go
@@ -0,0 +1,38 @@
+package aggregate_test
+
+import (
+	"testing"
+
+	"github.com/wenealves10/microservice-golang-k8s-ci-cd/aggregate"
+)
+
+func TestCustomer_NewCustomer(t *testing.T) {
+	type testCase struct {
+		test          string
+		name          string
+		expectedError error
+	}
+
+	testCases := []testCase{
+		{
+			test:          "Empty Name Validation",
+			name:          "",
+			expectedError: aggregate.ErrInvalidPerson,
+		},
+		{
+			test:          "Valid Name",
+			name:          "John Doe",
+			expectedError: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.test, func(t *testing.T) {
+			_, err := aggregate.NewCustomer(tc.name)
+			if err != tc.expectedError {
+				t.Errorf("Expected error %v, got %v", tc.expectedError, err)
+			}
+		})
+	}
+
+}
